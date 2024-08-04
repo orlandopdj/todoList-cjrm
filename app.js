ebea984c7a2f578d025a1c15740a2fc0cfad8f79
orlandopdj <orlandopdjr6@gmail.com>
2024-08-04 13:53:07 -0300

    ajustes finais

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const todosContainer = document.querySelector('ul')
 formTodo.addEventListener('submit', e => {
     e.preventDefault()
     const inputValue = e.target.add.value
-    todosContainer.innerHTML += `<li class="flex item items-center justify-between px-5 py-1 my-2 bg-slate-700">
-                <span class="text-gray-300">${inputValue}</span>
-                <i class="far fa-trash-alt  text-gray-300"></i>
+    if (inputValue.length) {
+        todosContainer.classList.add('bg-slate-800')
+        todosContainer.innerHTML += `
+            <li class="flex item items-center justify-between px-5 py-1 my-2 bg-slate-700" data-item="${inputValue}">
+                    <span class="text-gray-300">${inputValue}</span>
+                    <i class="far fa-trash-alt cursor-pointer  text-gray-300" data-delete="${inputValue}"></i>
             </li>`
+    }
 
     e.target.reset()
 })
@@ -31,9 +35,23 @@ search.addEventListener('input', e => {
     if (lisArray.every(item => item.className.includes('hidden'))) {
         todosContainer.classList.add('hidden')
         todosContainer.classList.remove('flex')
+
     } else {
         todosContainer.classList.add('flex')
         todosContainer.classList.remove('hidden')
     }
 
+})
+
+todosContainer.addEventListener('click', e => {
+    const clickedDelete = e.target.dataset.delete
+    const todo = document.querySelector(`[data-item="${clickedDelete}"]`)
+    if (clickedDelete) {
+        todo.remove()
+    }
+    const arrayLis = Array.from(todosContainer.children)
+
+    if (arrayLis.length == 0) {
+        todosContainer.classList.remove('bg-slate-800')
+    }
 })
